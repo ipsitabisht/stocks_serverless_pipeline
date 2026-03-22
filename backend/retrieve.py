f45d6b28f2ee53ef7394593473cd73d8999d3e9c
Ipsita Bisht <ipsitabisht@gmail.com>
2026-03-21 19:50:29 -0700

    adding files

---
+++ backend/retrieve.py
@@ -0,0 +1,6 @@
+import os 
+
+massive_api_key = os.getenv("MASSIVE_API_KEY")
+
+def retrieve_data(api_key: str, query: str) -> str:
+    pass
---
+++ frontend/index.js
@@ -0,0 +1,3 @@
+const handler = async () => {
+    return 'Hello World';
+};
